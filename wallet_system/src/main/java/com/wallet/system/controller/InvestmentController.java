6a850b7dfeefc7a1694782ea19c4fb487b96ec4c
ZetaCube <ZetaCube@DESKTOP-GSV6L5Q>
2023-12-06 12:03:10 +0900

    [수정]사용안하는 모듈 제거

--- wallet_system/src/main/java/com/wallet/system/controller/InvestmentController.java
+++ wallet_system/src/main/java/com/wallet/system/controller/InvestmentController.java
@@ -38,14 +38,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
